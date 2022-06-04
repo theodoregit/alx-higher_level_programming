068637e2322a4d43f442e050821d42f136b4ca4c
theodoregit <tedoteams@gmail.com>
2022-06-04 08:56:13 -0700

    add_all_args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from sys import argv
     add = 0
     for s in argv[1:]:
-    add += int(s)
-    print("{:d}".format(add))
+        add += int(s)
+        print("{:d}".format(add))
