8ffa82c6558dd4f8ccc170d03394fdfa597cb830
theodoregit <tedoteams@gmail.com>
2022-07-08 12:03:04 -0700

    print_int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     """Print an integer with "{:d}".format().
     Args:
         value (int): The integer to print.
-    
+
     Returns:
         If a TypeError or ValueError occurs - False.
         Otherwise - True.
